ca08f5cfa40a2baf88bc55a0847a9927270d80b3
Andrey Sidorov <sidorares@yandex.com>
2016-05-23 17:47:41 +1000

    test disqus

--- build-scripts/index.js
+++ build-scripts/index.js
@@ -55,6 +55,26 @@ var html = `
     <article>
     ${body}
     </article>
+    <div id="disqus_thread"></div>
+    <script>
+      /**
+      * RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+      * LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables
+      */
+      /*
+      var disqus_config = function () {
+        this.page.url = "https://andreysidorov.com"; // Replace PAGE_URL with your page's canonical URL variable
+        this.page.identifier = "index"; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+      };
+      */
+      (function() { // DON'T EDIT BELOW THIS LINE
+      var d = document, s = d.createElement('script');
+        s.src = '//andreysidorov.disqus.com/embed.js';
+        s.setAttribute('data-timestamp', +new Date());
+        (d.head || d.body).appendChild(s);
+      })();
+    </script>
+    <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript" rel="nofollow">comments powered by Disqus.</a></noscript>
   </body>
 </html>
 `;
